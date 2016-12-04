cb63e3633165da03c260fcd6a77447e780df0cef
vargaslo <vargaslo@gmail.com>
2016-12-04 01:20:33 -0600

    fixed for triclinic systems

--- xyz_from_slv.py
+++ xyz_from_slv.py
@@ -13,15 +13,33 @@ import shutil
 import sys
 import tempfile
 
-infile = sys.argv[1]
+
+def frac2xyzM(crystal_prms):
+
+    def cosd(deg):
+        return np.cos(deg/180. * np.pi)
+
+    def sind(deg):
+        return np.sin(deg/180. * np.pi)
+
+    a = crystal_prms['a']
+    b = crystal_prms['b']
+    c = crystal_prms['c']
+    alpha = crystal_prms['alpha']
+    beta = crystal_prms['beta']
+    gamma = crystal_prms['gamma']
+    v = np.sqrt(1-cosd(alpha)**2-cosd(beta)**2-cosd(gamma)**2 + 2*cosd(alpha)* cosd(beta)*cosd(gamma))
+    r1 = [a, b*cosd(gamma), c*cosd(beta)]
+    r2 = [0, b*sind(gamma), c*(cosd(alpha)-cosd(beta)*cosd(gamma))/sind(gamma)]
+    r3 = [0, 0, c*v/sind(gamma)]
+    M = np.array([r1, r2, r3])
+    return M
+
 
 def slv2xyz(infile):
     with open(infile, 'r') as fin:
         with open('myout.xyz', 'w') as fout:
 
-            a = None
-            b = None
-            c = None
             Nx = None
             Ny = None
             Nz = None
@@ -33,15 +51,26 @@ def slv2xyz(infile):
                 # Get grid specs
                 if line[0:4]=='CELL':
                     _, a, b, c, alpha, beta, gamma = line.split()
-                    a,b,c = map(float, [a,b,c])
+                    a,b,c, alpha, beta, gamma = map(float, [a,b,c, alpha, beta, gamma])
+
+                    # tranformation matrix to convert fractional to Cartesian
+                    crystal = {}
+                    crystal['a'] = a
+                    crystal['b'] = b
+                    crystal['c'] = c
+                    crystal['alpha'] = alpha
+                    crystal['beta'] = beta
+                    crystal['gamma'] = gamma
+                    M = frac2xyzM(crystal)
 
                 if line[0:4]=='SIZE':
                     _, Nx, Ny, Nz = line.split()
                     Nx,Ny,Nz = map(int, [Nx,Ny,Nz])
 
-                    xarr_edges = np.linspace(0, a, Nx+1)
-                    yarr_edges = np.linspace(0, b, Ny+1)
-                    zarr_edges = np.linspace(0, c, Nz+1)
+                    # fractional coordinates
+                    xarr_edges = np.linspace(0, 1, Nx+1)
+                    yarr_edges = np.linspace(0, 1, Ny+1)
+                    zarr_edges = np.linspace(0, 1, Nz+1)
 
                     xarr = 0.5 * (xarr_edges[1:] + xarr_edges[:-1])
                     yarr = 0.5 * (yarr_edges[1:] + yarr_edges[:-1])
@@ -54,7 +83,8 @@ def slv2xyz(infile):
                 if len(chars)==Nx:
                     for i, char in enumerate(chars):
                         if char=='1':
-                            fout.write('{} {} {} {}\n'.format('C', xarr[i], yarr[j%Ny], zarr[j//Ny]))
+                            x,y,z = np.dot(M, np.array([xarr[i], yarr[j%Ny], zarr[j//Ny]]))
+                            fout.write('{} {} {} {}\n'.format('C', x, y, z))
                             ones +=1
                     j+=1
     return ones
@@ -75,5 +105,6 @@ def prepend(string, infile):
     return
 
 
+infile = sys.argv[1]
 ones = slv2xyz(infile)
 prepend('{}'.format(ones), 'myout.xyz')
